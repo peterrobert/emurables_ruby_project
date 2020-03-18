66d9168c6044c73c6f769bea0b226c255d33b155
peterrobert <pwambui93@gmail.com>
2020-03-18 22:25:31 +0300

    my_each completed

--- main.rb
+++ main.rb
@@ -1,90 +1,18 @@
 module  Enumerable
 
-  def my_self 
-   
+  def my_each 
+    return to_enum :my_each unless block_given?
     item = self;
     i = 0
-
     while i < item.length
-
       if item.is_a?(Array)
- 
-        yield(item[i])
-
+        yield(item[i]) 
         i += 1
- 
       elsif item.is_a?(Hash)
-
-        yield(item.keys[i], item[item.keys[i]])
-
+        yield item.keys[i],item[item.keys[i]]
         i += 1
-
       else
         yield(item)
       end 
     end
   end
-
-
-  def my_each_with_index 
-
-    item = self;
-    i = 0
-
-    while i < item.length
-
-      if item.is_a?(Array)
- 
-        yield([i],item[i])
-
-        i += 1
- 
-      elsif item.is_a?(Hash)
-
-        yield([i],item.keys[i], item[item.keys[i]])
-
-        i += 1
-
-      else
-
-        yield(item)
-
-      end 
-    end
-
-
-  end
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  test = ["peter","robert","hey there","mary"];
-   
-  test_it = {
-   "name" => "peter",
-    "hate" => "peter",
-    "hey" => "peter",
-    "why" => "peter",
-  };
-
-  test_hell = "hey there"
-
-
-
-  puts test_it.my_each_with_index
-end
