45a8f56bda3cd99a4fe46dc23cfbb5c68d63821e
peterrobert <pwambui93@gmail.com>
2020-03-18 22:26:59 +0300

    My_each_with_index finished

--- main.rb
+++ main.rb
@@ -16,3 +16,26 @@ module  Enumerable
       end 
     end
   end
+
+  def my_each_with_index 
+    return to_enum :my_each unless block_given?
+    item = self;
+    i = 0
+    while i < item.length
+
+      if item.is_a?(Array)
+ 
+        yield [i],item[i]
+
+        i += 1
+ 
+      elsif item.is_a?(Hash)
+        yield [i],item.keys[i], item[item.keys[i]] 
+        i += 1
+      else
+        yield(item)
+      end 
+    end
+  end
+  
+  
