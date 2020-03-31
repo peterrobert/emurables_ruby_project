f3bbeac628ed65a4477f0de0cd26bf078593171f
peterrobert <pwambui93@gmail.com>
2020-03-31 19:18:06 +0300

    improved my each method

--- main.rb
+++ main.rb
@@ -1,19 +1,18 @@
 module  Enumerable
 
-  def my_each 
+  def my_each ( &parm )
     return to_enum :my_each unless block_given?
     item = self;
     i = 0
     while i < item.length
       if item.is_a?(Array)
         yield(item[i]) 
-        i += 1
-      elsif item.is_a?(Hash)
-        yield item.keys[i],item[item.keys[i]]
-        i += 1
+      elsif parm.arity == 1
+        yield  ( assoc item.keys[i] ) 
       else
-        yield(item)
+        yield item.keys[i] , item[item.keys[i]]
       end 
+      i += 1
     end
   end
 
@@ -62,4 +61,50 @@ module  Enumerable
     end
   end
 
-  
+  def my_all(argument = nil)
+    if argument.nil? && !block_given?
+      for value in self do
+        matches = value.nil? || !value == true
+        break if matches
+      end
+      !matches
+    elsif argument.nil?
+      for value in self do
+        matches = yield(value)
+        break unless matches
+      end
+      matches
+    elsif argument.is_a?(Regexp)
+      for value in self do
+        matches = value.to_s.match?(argument)
+        break unless matches
+      end
+      matches
+    elsif argument.is_a?(Class)
+      for value in self do
+        matches = value.is_a?(argument)
+        break unless matches
+      end
+      matches
+    else
+      for value in self do
+        matches = value == argument
+        break unless matches
+      end
+      matches
+    end
+  end
+end  
+a = {
+  "a" => 9,
+  "b" => 8,
+  "c" => 0,
+  "d" => 4,
+  "e" => 97,
+  "f" => 82
+}
+a.my_each do |i , k|
+
+
+end
+
